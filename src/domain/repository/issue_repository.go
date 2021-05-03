9700f13b33ecbf70ef7047cb7842259f7cc15a08
TJ-Yusuke <jeeproud@gmail.com>
2021-05-03 18:46:02 +0900

    repository修正

--- src/domain/repository/issue_repository.go
+++ src/domain/repository/issue_repository.go
@@ -2,12 +2,9 @@ package repository
 
 import (
 	"github-gantt-api/src/domain/entity"
-	"time"
 )
 
 type IssueRepository interface {
 	GetIssues(projectId uint16) ([]*entity.Issue, error)
-	SetStartDate(date time.Time, issue *entity.Issue) error
-	SetDueDate(date time.Time, issue *entity.Issue) error
-	SetProgress(progress uint8, issue *entity.Issue) error
+	UpdateIssue(issue *entity.Issue) error
 }
