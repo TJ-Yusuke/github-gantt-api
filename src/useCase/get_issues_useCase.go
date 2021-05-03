a833069c85a9be6175aafd3a5ad16a5081b7f388
TJ-Yusuke <jeeproud@gmail.com>
2021-05-03 18:46:31 +0900

    エラーハンドリング修正

--- src/useCase/get_issues_useCase.go
+++ src/useCase/get_issues_useCase.go
@@ -1,7 +1,7 @@
 package useCase
 
 import (
-	"errors"
+	"fmt"
 	"github-gantt-api/src/domain/entity"
 	"github-gantt-api/src/domain/repository"
 )
@@ -19,7 +19,7 @@ func NewGetIssuesUseCase(repository repository.IssueRepository) *GetIssuesUseCas
 func (giu *GetIssuesUseCase) GetIssues(projectId uint16) ([]*entity.Issue, error) {
 	issues, err := giu.repository.GetIssues(projectId)
 	if err != nil {
-		return nil, errors.New("could not get issues via github api")
+		return nil, fmt.Errorf("could not get issues via github api of '%v'", err)
 	}
 	return issues, nil
 }
