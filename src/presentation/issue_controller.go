8bfd6782538b61b1890744b95143caf84a61db5f
TJ-Yusuke <jeeproud@gmail.com>
2021-05-05 22:39:01 +0900

    issueのプレゼンターを追加 & issue一覧取得メソッド追加

---
+++ src/presentation/issue_controller.go
@@ -0,0 +1,30 @@
+package presentation
+
+import (
+	"fmt"
+	"github-gantt-api/src/domain/entity"
+	"github-gantt-api/src/usecase"
+	"sort"
+)
+
+type IssueController struct {
+	useCase usecase.IssueUseCase
+}
+
+func NewIssueController(useCase usecase.IssueUseCase) *IssueController {
+	ic := new(IssueController)
+	ic.useCase = useCase
+	return ic
+}
+
+func (ic *IssueController) GetIssues(projectId uint16) []*entity.Issue {
+	issues, err := ic.useCase.GetIssues(projectId)
+	if err != nil {
+		fmt.Errorf("could not get Issues because of '%v'", err)
+	}
+	// 期日の最新順に並び替える
+	sort.Slice(issues, func(i, j int) bool {
+		return issues[i].DueDate.Before(issues[j].DueDate)
+	})
+	return issues
+}
