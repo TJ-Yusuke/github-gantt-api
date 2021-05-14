23455660f7d9df8df8b289745e245d26ab575dea
TJ-Yusuke <jeeproud@gmail.com>
2021-05-14 13:09:06 +0900

    startDate追加

--- src/presentation/issue_controller.go
+++ src/presentation/issue_controller.go
@@ -1,9 +1,11 @@
 package presentation
 
 import (
+	"encoding/binary"
 	"encoding/json"
 	"fmt"
 	"github-gantt-api/src/usecase"
+	"time"
 )
 
 type IssueController struct {
@@ -17,7 +19,7 @@ func NewIssueController(useCase usecase.IssueUseCase) *IssueController {
 }
 
 func (ic *IssueController) GetIssues(projectId uint16) (response []byte, error error) {
-	issues, err := ic.useCase.GetIssues(projectId)
+	issues, err := ic.useCase.GetIssuesList(projectId)
 	if err != nil {
 		return nil, fmt.Errorf("could not get Issues because of '%v'", err)
 	}
@@ -26,4 +28,21 @@ func (ic *IssueController) GetIssues(projectId uint16) (response []byte, error e
 		return nil, fmt.Errorf("convert json error '%v'", err)
 	}
 	return res, nil
+}
+func (ic *IssueController) SetStartDate(date []byte, oldIssueIdByte []byte) error {
+	oldIssueId := binary.BigEndian.Uint16(oldIssueIdByte)
+	oldIssue, err := ic.useCase.GetIssue(oldIssueId)
+	if err != nil {
+		return fmt.Errorf("failed to fetch issue %v", err)
+	}
+	layout := "Mon Jan 2 15:04:05 MST 2006"
+	startDate, err := time.Parse(layout, string(date))
+	if err != nil {
+		return fmt.Errorf("failed to convert string to Time.time '%v'", err)
+	}
+	err2 := ic.useCase.SetStartDate(startDate, oldIssue)
+	if err2 != nil {
+		return fmt.Errorf("failed to setStartDate '%v'", err2)
+	}
+	return nil
 }
