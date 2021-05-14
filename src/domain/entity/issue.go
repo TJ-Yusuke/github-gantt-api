5b72d65a28ed5eb4d4f8746b5b0623e77f52424f
TJ-Yusuke <jeeproud@gmail.com>
2021-05-14 11:11:15 +0900

    labelをrepoの移動のためissueを修正

--- src/domain/entity/issue.go
+++ src/domain/entity/issue.go
@@ -2,7 +2,6 @@ package entity
 
 import (
 	"errors"
-	"github-gantt-api/src/domain/valueObject"
 	"time"
 )
 
@@ -10,25 +9,25 @@ type Issue struct {
 	Id        uint16
 	Title     string
 	Url       string
-	Repo      valueObject.Repo
-	Label     valueObject.Label
+	RepoId    uint16
+	LabelId   uint16
 	StartDate time.Time
 	DueDate   time.Time
 	Progress  uint8
 	Assignee  string
 }
 
-func NewIssue(id uint16, title string, url string, repo valueObject.Repo) *Issue {
+func NewIssue(id uint16, title string, url string, repoId uint16) *Issue {
 	issue := new(Issue)
 	issue.Id = id
 	issue.Title = title
 	issue.Url = url
-	issue.Repo = repo
+	issue.RepoId = repoId
 	return issue
 }
 
-func (issue *Issue) SetLabel(label valueObject.Label) *Issue {
-	issue.Label = label
+func (issue *Issue) SetLabel(labelId uint16) *Issue {
+	issue.LabelId = labelId
 	return issue
 }
 
@@ -37,7 +36,7 @@ func (issue *Issue) SetStartDate(date time.Time) *Issue {
 	return issue
 }
 
-// 期日は開始日よりも前に設定することはできない
+// SetDueDate 期日は開始日よりも前に設定することはできない
 func (issue *Issue) SetDueDate(date time.Time) (*Issue, error) {
 	if issue.StartDate.Before(date) || issue.StartDate.Equal(date) {
 		issue.DueDate = date
@@ -47,7 +46,7 @@ func (issue *Issue) SetDueDate(date time.Time) (*Issue, error) {
 	}
 }
 
-// 進捗は常に0~100の間の整数
+// SetProgress 進捗は常に0~100の間の整数
 func (issue *Issue) SetProgress(progress uint8) (*Issue, error) {
 	if progress <= 100 {
 		issue.Progress = progress
