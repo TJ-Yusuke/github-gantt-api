76a179c059e567203f82099d163366d11a18853b
TJ-Yusuke <jeeproud@gmail.com>
2021-05-16 16:57:05 +0900

    context追加

--- src/domain/repository/issue_repository.go
+++ src/domain/repository/issue_repository.go
@@ -1,12 +1,13 @@
 package repository
 
 import (
+	"context"
 	"github-gantt-api/src/domain/entity"
 )
 
 type IssueRepository interface {
 	GetIssue(issueId uint16) (*entity.Issue, error)
 	// GetIssuesList 開始日と期日が登録されているものしかとってこない
-	GetIssuesList(projectNumber uint16, org string) ([]*entity.Issue, error)
+	GetIssuesList(ctx context.Context, projectNumber uint16, org string) ([]*entity.Issue, error)
 	UpdateIssue(issue *entity.Issue) error
 }
